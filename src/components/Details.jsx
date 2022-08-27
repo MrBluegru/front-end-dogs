66c4f82f0818f4a5a01239adeae9be1054280e87
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-08-27 14:48:12 -0300

    V

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDogsId } from "../redux/action";
 import { Link, useParams } from "react-router-dom";
-import Loading from "../components/Loading"
+import Loading from "../components/Loading";
+import ErrorC from "../components/Error";
 import "../styles/details.css";
 
 const Details = () => {
@@ -14,47 +15,58 @@ const Details = () => {
     dispach(getDogsId(id));
   }, [dispach, id]);
 
-  return (
-    <div className="detailsD">
-      {Object.entries(dogsDet).length ?(<div className="detaisS">
-        <div className="titleBtn">
-          <Link to={"/home"}>
-            <button className="btnB">↩</button>
-          </Link>
-          <p className="nameTitle">{dogsDet.name}</p>
-        </div>
-        <img
-          className="imgD"
-          src={dogsDet.image}
-          alt={`Pic of ${dogsDet.name}`}
-        />
-        <div className="hAndW">
+  
+  if (dogsDet !== `Non-existent`) {
+    return (
+      <div className="detailsD">
+        {Object.entries(dogsDet).length ? (
+          <div className="detaisS">
+            <div className="titleBtn">
+              <Link to={"/home"}>
+                <button className="btnB">↩</button>
+              </Link>
+              <p className="nameTitle">{dogsDet.name}</p>
+            </div>
+            <img
+              className="imgD"
+              src={dogsDet.image}
+              alt={`Pic of ${dogsDet.name}`}
+            />
+            <div className="hAndW">
+              <div className="hDg">
+                <p>cm. {dogsDet.minHeightCm}</p>
+                <p className="hwText">⇣ Height ⇡</p>
+                <p>cm. {dogsDet.maxHeightCm}</p>
+              </div>
 
-          <div className="hDg">
-            <p>cm. {dogsDet.minHeightCm}</p>
-            <p className="hwText">⇣ Height ⇡</p>
-            <p>cm. {dogsDet.maxHeightCm}</p>
+              <div className="wDg">
+                <p>kg. {dogsDet.minWeightKg}</p>
+                <p className="hwText">▼ Weight ▲</p>
+                <p>kg. {dogsDet.maxWeightKg}</p>
+              </div>
+            </div>
+
+            <p className="temperText">Temperament</p>
+            <div className="temperG">
+              {dogsDet.temperament?.map((e) => {
+                return <p> ✔ {e}</p>;
+              })}
+            </div>
+            <div className="lifeSD">
+              <p>
+                Life Span {dogsDet.minLifeSpanYears} -{" "}
+                {dogsDet.maxLifeSpanYears} years
+              </p>
+            </div>
           </div>
-
-          <div className="wDg">
-            <p>kg. {dogsDet.minWeightKg}</p>
-            <p className="hwText">▼ Weight ▲</p>
-            <p>kg. {dogsDet.maxWeightKg}</p>
-          </div>
-        </div>
-
-        <p className="temperText">Temperament</p>
-        <div className="temperG">
-          {dogsDet.temperament?.map((e) => {
-            return <p> ✔ {e}</p>;
-          })}
-        </div>
-        <div className="lifeSD">
-          <p>Life Span {dogsDet.minLifeSpanYears} - {dogsDet.maxLifeSpanYears} years</p>
-        </div>
-      </div>):<Loading/>}
-    </div>
-  );
+        ) : (
+          <Loading />
+        )}
+      </div>
+    );
+  } else {
+    return <ErrorC />;
+  }
 };
 
 export default Details;
