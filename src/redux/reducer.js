877675f080f558065b72cbe53ccfd09ef8361b18
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-08-27 13:34:25 -0300

    IV

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -41,21 +41,21 @@ export default function rootReducer(state = initialState, action) {
       const orderBN =
         action.payload === "asc"
           ? [...state.dogs].sort(function (a, b) {
-              if (a.name > b.name) {
+              if (a.name.toLowerCase() > b.name.toLowerCase()) {
                 return 1;
               }
 
-              if (b.name > a.name) {
+              if (b.name.toLowerCase() > a.name.toLowerCase()) {
                 return -1;
               }
               return 0;
             })
           : [...state.dogs].sort(function (a, b) {
-              if (a.name > b.name) {
+              if (a.name.toLowerCase() > b.name.toLowerCase()) {
                 return -1;
               }
 
-              if (b.name > a.name) {
+              if (b.name.toLowerCase() > a.name.toLowerCase()) {
                 return 1;
               }
               return 0;
