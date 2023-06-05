c81109fa6882b3679575ea6019268d3ee7112804
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-06-05 20:37:41 -0300

    Update

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,18 +7,15 @@ import React from "react";
 const Landing = () => {
   return (
     <div className="landing">
-      <div className="welc">
+      <div className="title">
         <p>Welcome to Dogs App</p>
       </div>
 
-      <div className="divGo">
-        <Link to={`/home`}>
-          <button className="go">Start</button>
-        </Link>
-      </div>
+      <Link to={`/home`}>
+        <button className="btn_go">Start</button>
+      </Link>
 
       <a
-        className="linkA"
         href="https://www.linkedin.com/in/mrbluegru/"
         target="_blank"
         rel="noreferrer"
