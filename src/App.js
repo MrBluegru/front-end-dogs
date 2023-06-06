44b341868f9faaf7e04c389ae844d03a414a53c2
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-06-05 21:54:36 -0300

    Update: quit styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import "./App.css";
 import Landing from "./components/Landing";
 import ErrorC from "./components/Error";
 import FormCreate from "./components/FormCreate";
 import Details from "./components/Details";
 import CreateOk from "./components/CreateOk"
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
@@ -19,7 +19,7 @@ function App() {
           <Route path="*" element={<ErrorC />} />
           
         </Routes>
-      </div>
+      </>
     </BrowserRouter>
   );
 }
