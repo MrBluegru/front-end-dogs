1e06ddcd11b4ab7120be657dcda9e581fe007939
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-06-09 11:03:58 -0300

    Update

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/errorC.css"
+import "../styles/errorC.css";
 
 const ErrorC = () => {
   return (
     <div className="errorC">
-      <p className="nro1">Page Not Found</p>
-      <p className="nro2">We couldn't find what you were looking for.</p>
-      <Link to={`/home`}>
-        <button className="nro3">Go back</button>
-      </Link>
+      <div className="blurDiv-errorC">
+        <p className="title-errorC">Page Not Found</p>
+        <p className="message-errorC">
+          We couldn't find what you were looking for.
+        </p>
+        <Link to={`/home`}>
+          <button className="goBack-errorC">Go back</button>
+        </Link>
+      </div>
     </div>
   );
 };
