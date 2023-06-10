8f20a0429e391626b90f32989f7f4847b567c37c
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-06-10 16:47:09 -0300

    Update styles

--- src/components/CreateOk.jsx
+++ src/components/CreateOk.jsx
@@ -5,17 +5,21 @@ import "../styles/createdOk.css";
 const CreateOk = () => {
   let navigate = useNavigate();
 
-  function handleClick(e) {
-    navigate("/createNewDog", { replace: true });
-  }
   return (
     <div className="createOk">
-      <p className="newD">New dog added successfully</p>
-      <div className="btns">
-        <Link to={`/home`}>
-          <button className="btnGoH">Go back home</button>
-        </Link>
-          <button className="btnAdd" onClick={(e) => handleClick(e)}>Add 1+</button>
+      <div className="back-createOk">
+        <p className="newD">New dog added successfully</p>
+        <div className="btns">
+          <Link to={`/home`}>
+            <button className="btnGoH">Go back home</button>
+          </Link>
+          <button
+            className="btnAdd"
+            onClick={() => navigate("/createNewDog", { replace: true })}
+          >
+            Add 1+
+          </button>
+        </div>
       </div>
     </div>
   );
