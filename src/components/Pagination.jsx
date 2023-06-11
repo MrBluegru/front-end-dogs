74ee9e6969c52aeee2d73135f7cf8b1082ec96b5
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-06-11 13:46:12 -0300

    Update type of pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,52 @@
-import React from "react";
+// import React from "react";
+// import "../styles/pagination.css";
+
+// export default function Pagination({
+//   dogsPerPage,
+//   allDogs,
+//   paginado,
+//   currentPage,
+// }) {
+//   const pageNumbers = [];
+
+//   for (let i = 1; i <= Math.ceil(allDogs / dogsPerPage); i++) {
+//     pageNumbers.push(i);
+//   }
+
+//   let pageLength = pageNumbers.length;
+
+//   return (
+//     <div className="page-item">
+//       <button
+//         className="nex-previous"
+//         onClick={() =>
+//           paginado(currentPage === 1 ? currentPage : currentPage - 1)
+//         }
+//       >{`🢤`}</button>
+
+//       {pageNumbers?.map((e) => {
+//         return (
+//           <button
+//             onClick={() => paginado(e)}
+//             key={e}
+//             className={e === currentPage ? "current" : "rest"}
+//           >
+//             {e}
+//           </button>
+//         );
+//       })}
+
+//       <button
+//         className="nex-previous"
+//         onClick={() =>
+//           paginado(currentPage === pageLength ? currentPage : currentPage + 1)
+//         }
+//       >{`🢥`}</button>
+//     </div>
+//   );
+// }
+
+import React, { useState, useEffect } from "react";
 import "../styles/pagination.css";
 
 export default function Pagination({
@@ -13,35 +61,74 @@ export default function Pagination({
     pageNumbers.push(i);
   }
 
-  let pageLength = pageNumbers.length;
+  const pageLength = pageNumbers.length;
+  const [inputValue, setInputValue] = useState(currentPage.toString());
+
+  useEffect(() => {
+    setInputValue(currentPage.toString());
+  }, [currentPage]);
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    if (
+      !isNaN(value) &&
+      parseInt(value) >= 1 &&
+      parseInt(value) <= pageLength
+    ) {
+      setInputValue(value);
+    }
+  };
+
+  const handleSubmit = () => {
+    if (inputValue !== "") {
+      paginado(parseInt(inputValue));
+      setInputValue("");
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
 
   return (
     <div className="page-item">
       <button
-        className="nex-previous"
+        className={currentPage === 1 ? "blocked" : "nex-previous"}
         onClick={() =>
           paginado(currentPage === 1 ? currentPage : currentPage - 1)
         }
-      >{`🢤`}</button>
+        disabled={currentPage === 1}
+      >
+        {"🢤"}
+      </button>
 
-      {pageNumbers?.map((e) => {
-        return (
-          <button
-            onClick={() => paginado(e)}
-            key={e}
-            className={e === currentPage ? "current" : "rest"}
-          >
-            {e}
-          </button>
-        );
-      })}
+      <div className="inpt-text-pagination">
+        <input
+          type="text"
+          className="input-paginationC"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
+
+        <p style={{ fontWeight: "bold" }}>{`of ${pageLength}`}</p>
+      </div>
+
+      <button className="btn-go-pagination" onClick={handleSubmit}>
+        Go
+      </button>
 
       <button
-        className="nex-previous"
+        className={currentPage === pageLength ? "blocked" : "nex-previous"}
         onClick={() =>
           paginado(currentPage === pageLength ? currentPage : currentPage + 1)
         }
-      >{`🢥`}</button>
+        disabled={currentPage === pageLength}
+      >
+        {"🢥"}
+      </button>
     </div>
   );
 }
